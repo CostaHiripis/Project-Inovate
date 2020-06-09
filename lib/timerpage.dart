022ce4cc089239aa9816cfb94394cadf1c6107bb
Costa <CostaHiripis@users.noreply.github.com>
2020-06-09 10:25:50 +0200

    Update timerpage.dart

--- lib/timerpage.dart
+++ lib/timerpage.dart
@@ -5,111 +5,108 @@ class TimerPage extends StatefulWidget {
   @override
   _TimerPageState createState() => _TimerPageState();
 }
+
 class _TimerPageState extends State<TimerPage> {
+  bool isVisible = false;
+  bool running = false;
+  String buttonStartStopResume = "Start";
+  String stopTimeToDisplay = "00:00:00";
+  var sWatch = Stopwatch();
+  final dur = const Duration(seconds: 1);
 
-bool isVisible = false;
-bool running = false;
-String buttonStartStopResume = "Start";
-String stopTimeToDisplay = "00:00:00";
-var sWatch = Stopwatch();
-final dur = const Duration(seconds: 1);
-
-void startTimer()
-{
-  Timer(dur, keepRunning);
-}
-
-void keepRunning()
-{
-  if(sWatch.isRunning)
-  {
-    startTimer();
+  void startTimer() {
+    Timer(dur, keepRunning);
   }
-  setState(() {
-     stopTimeToDisplay = sWatch.elapsed.inHours.toString().padLeft(2, "0") + ":" + 
-  (sWatch.elapsed.inMinutes%60).toString().padLeft(2, "0") + ":" + 
-  (sWatch.elapsed.inSeconds%60).toString().padLeft(2, "0");
-  });
- 
-}
 
-void startStopwatch()
-{
-  sWatch.start();
-  startTimer();
-}
+  void keepRunning() {
+    if (sWatch.isRunning) {
+      startTimer();
+    }
+    setState(() {
+      stopTimeToDisplay = sWatch.elapsed.inHours.toString().padLeft(2, "0") +
+          ":" +
+          (sWatch.elapsed.inMinutes % 60).toString().padLeft(2, "0") +
+          ":" +
+          (sWatch.elapsed.inSeconds % 60).toString().padLeft(2, "0");
+    });
+  }
 
-void stopStopwatch()
-{
-  sWatch.stop();
-}
+  void startStopwatch() {
+    sWatch.start();
+    startTimer();
+    setState(() {
+      running = true;
+      buttonStartStopResume = "Stop";
+    });
+  }
 
-void resetStopwatch()
-{
-  sWatch.reset();
-  stopTimeToDisplay = "00:00:00";
-}
+  void stopStopwatch() {
+    sWatch.stop();
+    setState(() {
+      running = false;
+      buttonStartStopResume = "Resume";
+      isVisible = true;
+    });
+  }
 
+  void resetStopwatch() {
+    sWatch.reset();
+    stopTimeToDisplay = "00:00:00";
+    setState(() {
+      running = false;
+      buttonStartStopResume = "Start";
+      isVisible = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              flex: 6,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  stopTimeToDisplay,
-                  style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w700),
-                ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                stopTimeToDisplay,
+                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w700),
               ),
             ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        RaisedButton(
-                          child: Text(
-                            '$buttonStartStopResume',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          color: running ? Colors.red : Colors.green,
-                          onPressed: () {
-                            try {
-                              if (running == false && buttonStartStopResume == "Start") {
-                                setState(() {
-                                  running = true;
-                                  buttonStartStopResume = "Stop";
-                                  startStopwatch();
-                                });
-                              } else if (running == true && buttonStartStopResume == "Stop") {
-                                setState(() {
-                                  running = false;
-                                  buttonStartStopResume = "Resume";
-                                  stopStopwatch();
-                                  isVisible = true;
-                                });
-                              } else if (running == false && buttonStartStopResume == "Resume") {
-                                setState(() {
-                                  running = true;
-                                  buttonStartStopResume = "Stop";
-                                  startStopwatch();
-                                });
-                              }
-                            } on Exception {}
-                          },
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text(
+                          '$buttonStartStopResume',
+                          style: TextStyle(color: Colors.white),
                         ),
-                        Visibility(
-                          visible: isVisible,
-                          child: 
-                        RaisedButton(
+                        color: running ? Colors.red : Colors.green,
+                        onPressed: () {
+                          try {
+                            if (running == false &&
+                                buttonStartStopResume == "Start") {
+                              startStopwatch();
+                            } else if (running == true &&
+                                buttonStartStopResume == "Stop") {
+                              stopStopwatch();
+                            } else if (running == false &&
+                                buttonStartStopResume == "Resume") {
+                              startStopwatch();
+                            }
+                          } on Exception {}
+                        },
+                      ),
+                      Visibility(
+                        visible: isVisible,
+                        child: RaisedButton(
                           child: Text(
                             'Reset',
                             style: TextStyle(color: Colors.white),
@@ -117,26 +114,22 @@ void resetStopwatch()
                           color: Colors.blue,
                           onPressed: () {
                             try {
-                              if (running == false && buttonStartStopResume == "Resume") {
-                                setState(() {
-                                  running = false;
-                                  buttonStartStopResume = "Start";
-                                  resetStopwatch();
-                                  isVisible = false;
-                                });
+                              if (running == false &&
+                                  buttonStartStopResume == "Resume") {
+                                resetStopwatch();
                               }
                             } on Exception {}
                           },
                         ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
