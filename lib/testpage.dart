b99ec70ac8dcb7d9e3d6a12d6d10e578e4d81dc5
Costa <CostaHiripis@users.noreply.github.com>
2020-06-17 13:12:44 +0200

    Update testpage.dart

--- lib/testpage.dart
+++ lib/testpage.dart
@@ -12,11 +12,8 @@ class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Yo what's good homie :)"),
-      ),
       body:
-          Text("This is just an empty page for now, nothing special... yet ;)"),
+          Text("Reminders"),
     );
   }
 }
